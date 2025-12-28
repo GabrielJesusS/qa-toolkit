173470ffdd6450f32ea2816e9d960bf7c9600023
Gabriel Jesus <gabrieljesus9221@gmail.com>
2025-12-28 12:02:35 -0300

    feat: adds basic http client

---
+++ src/core/HttpClient/index.ts
@@ -0,0 +1,70 @@
+import { HttpMethodsEnum } from "./HttpMethodsEnum";
+import HttpStatusCode from "./HttpStatusCodeEnum";
+
+type CustomHeaders =
+  | {
+      "Content-Type"?: string;
+      Authorization?: string;
+    }
+  | {};
+
+type HTTPClientCustomOptions = Omit<RequestInit, "headers" | "method"> & {
+  headers?: CustomHeaders;
+  method?: HttpMethodsEnum;
+};
+
+type HTTPClientExecutionReturn<T> = {
+  data: T;
+  status: HttpStatusCode;
+};
+
+const DEFAULT_HEADERS = { "Content-Type": "application/json" };
+
+const ERROR_STATUS_CODES = [
+  HttpStatusCode.BAD_REQUEST,
+  HttpStatusCode.UNAUTHORIZED,
+  HttpStatusCode.FORBIDDEN,
+  HttpStatusCode.NOT_FOUND,
+  HttpStatusCode.METHOD_NOT_ALLOWED,
+  HttpStatusCode.NOT_ACCEPTABLE,
+  HttpStatusCode.CONFLICT,
+  HttpStatusCode.UNPROCESSABLE_ENTITY,
+  HttpStatusCode.TOO_MANY_REQUESTS,
+  HttpStatusCode.INTERNAL_SERVER_ERROR,
+];
+
+function mountHeader(customHeader?: CustomHeaders) {
+  return { ...DEFAULT_HEADERS, ...customHeader };
+}
+
+async function parseResponse<T>(response: Response) {
+  if (response.status === HttpStatusCode.NO_CONTENT) {
+    return {} as T;
+  }
+
+  const parsedBody: T = await response.json();
+
+  if (ERROR_STATUS_CODES.includes(response.status)) {
+    throw new Error(
+      `HTTP Error: ${response.status}\n Message: ${JSON.stringify(parsedBody)}`
+    );
+  }
+
+  return parsedBody;
+}
+
+export async function client<T>(
+  url: string,
+  options?: HTTPClientCustomOptions
+): Promise<HTTPClientExecutionReturn<T>> {
+  const headers = mountHeader(options?.headers);
+
+  const response = await fetch(url, {
+    ...options,
+    headers,
+  });
+
+  const parsedResponse = await parseResponse<T>(response);
+
+  return { data: parsedResponse, status: response.status };
+}
