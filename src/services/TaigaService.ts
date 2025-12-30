0329a80715c5a5b98155dd7876e686962c4d66ba
Gabriel Jesus <gabrieljesus9221@gmail.com>
2025-12-30 13:39:59 -0300

    feat: adds refresh token into taiga service

--- src/services/TaigaService.ts
+++ src/services/TaigaService.ts
@@ -1,10 +1,16 @@
 import { client } from "@/core/HttpClient";
+import { HTTPError } from "@/core/HttpClient/HttpError";
 import { HttpMethodsEnum } from "@/core/HttpClient/HttpMethodsEnum";
+import HttpStatusCode from "@/core/HttpClient/HttpStatusCodeEnum";
 
 type TaigaTokenResponse = {
   refresh: string;
   auth_token: string;
 };
+
+let retries = 0;
+
+const maxRetries = 2;
 
 class TaigaService {
   static readonly tokenKey = "qtk_taiga_token";
@@ -14,6 +20,8 @@ class TaigaService {
   static readonly baseUrl = "https://api.taiga.io/api/v1";
 
   authToken: string | null = null;
+
+  refreshToken: string | null = null;
 
   async signIn(email: string, password: string): Promise<TaigaTokenResponse> {
     try {
@@ -37,11 +45,32 @@ class TaigaService {
 
   async signOut(): Promise<void> {}
 
-  async revalidateToken(): Promise<void> {}
+  async revalidateToken(): Promise<boolean> {
+    try {
+      const response = await client<TaigaTokenResponse>(
+        `${TaigaService.baseUrl}/auth/refresh`,
+        {
+          method: HttpMethodsEnum.POST,
+          body: JSON.stringify({
+            refresh: this.refreshToken,
+          }),
+        }
+      );
+
+      this.authToken = response.data.auth_token;
+      this.refreshToken = response.data.refresh;
+
+      retries = 0;
+
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
 
   async createIssue(): Promise<void> {}
 
-  async checkLogin() {
+  async checkLogin(): Promise<boolean | TaigaTokenResponse> {
     try {
       const response = await client<TaigaTokenResponse>(
         `${TaigaService.baseUrl}/users/me`,
@@ -55,8 +84,32 @@ class TaigaService {
 
       return response.data;
     } catch (error) {
+      if (!(error instanceof HTTPError)) {
+        throw new Error("Failed to connect to Taiga");
+      }
+
+      if (error.statusCode === HttpStatusCode.UNAUTHORIZED) {
+        return await this.#handleExpiredToken(
+          error,
+          async () => await this.checkLogin()
+        );
+      }
+
       return false;
     }
+  }
+
+  async #handleExpiredToken<T>(error: HTTPError, cb: () => T) {
+    const couldRevalidate = await this.revalidateToken();
+
+    if (couldRevalidate && retries < maxRetries) {
+      retries++;
+      const result = await cb();
+      return result;
+    }
+
+    retries = 0;
+    throw new Error("Failed to revalidate Taiga token");
   }
 }
 
