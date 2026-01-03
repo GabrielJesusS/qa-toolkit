d77a5251a2aa369d1b78dc4430e59e1a664c14c4
Gabriel Jesus <gabrieljesus9221@gmail.com>
2026-01-03 10:10:36 -0300

    feat: adds tabs component

---
+++ src/components/TabGroup.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { provide, ref, watch } from 'vue'
+
+interface Props {
+  modelValue?: string | number
+}
+
+const props = defineProps<Props>()
+
+const emit = defineEmits<{
+  'update:modelValue': [value: string | number]
+}>()
+
+const activeTab = ref(props.modelValue)
+
+watch(() => props.modelValue, (newValue) => {
+  activeTab.value = newValue
+})
+
+const setActiveTab = (value: string | number) => {
+  activeTab.value = value
+  emit('update:modelValue', value)
+}
+
+provide('activeTab', activeTab)
+provide('setActiveTab', setActiveTab)
+</script>
+
+<template>
+  <div class="qtk:w-full">
+    <slot />
+  </div>
+</template>
---
+++ src/components/TabPanel.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed, inject, type Ref } from 'vue'
+
+interface Props {
+  value: string | number
+}
+
+const props = defineProps<Props>()
+
+const activeTab = inject<Ref<string | number | undefined>>('activeTab')
+
+const isActive = computed(() => activeTab?.value === props.value)
+</script>
+
+<template>
+  <div v-if="isActive" class="qtk:py-4">
+    <slot />
+  </div>
+</template>
