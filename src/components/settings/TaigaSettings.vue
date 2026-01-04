3918174bceaadf648a77844befeb2bb559dfb7a8
Gabriel Jesus <gabrieljesus9221@gmail.com>
2026-01-04 10:28:56 -0300

    feat: adds default project id to taiga settings

--- src/components/settings/TaigaSettings.vue
+++ src/components/settings/TaigaSettings.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import { useTaigaSettings } from '@/composables/useTaigaSettings';
-import ProjectSelector from '../ProjectSelector.vue';
 import { useListProjects } from '@/composables/useListProjects';
+import Label from '../Label.vue';
+import Select from '../Select.vue';
+import Button from '../Button.vue';
 
 const { settings, setSettings } = useTaigaSettings();
 const { projects } = useListProjects();
+
+const resetProject = () => {
+    settings.value.defaultProjectId = '';
+    settings.value.defaultProjectName = '';
+    setSettings(settings.value);
+}
 
 
 
@@ -24,6 +32,16 @@ const onProjectChange = (event: Event) => {
 
 <template>
     <div class="qtk:space-y-4">
-        <ProjectSelector :value="settings.defaultProjectId" @change="onProjectChange" />
+        <div class="qtk:flex qtk:gap-4 qtk:items-end">
+            <div>
+                <Label for="issue-project">Select a default project</Label>
+                <Select :value="settings.defaultProjectId" @change="onProjectChange" v-bind="$attrs"
+                    :options="projects.map(project => ({ label: project.name, value: project.id }))" id="issue-project"
+                    placeholder="Select a project"></Select>
+            </div>
+            <Button type="button" @click="resetProject">
+                Reset
+            </Button>
+        </div>
     </div>
 </template>
