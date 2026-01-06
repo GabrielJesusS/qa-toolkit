e1b2ec977524348f09fc4686b94cdbe2560a8917
Gabriel Jesus <gabrieljesus9221@gmail.com>
2026-01-06 10:15:57 -0300

    feat: adds network tracking

--- src/core/ExtensionWorker.ts
+++ src/core/ExtensionWorker.ts
@@ -1,6 +1,24 @@
+import { AppConfigSchema } from "@/schemas/settings/app-config";
+import { StorageKeyEnum } from "./enums/StorageKeyEnum";
 import messageController from "./MessageController";
+import { StorageController } from "./StorageController";
+
+type TrackInfo = {
+  url: string;
+  createdAt: number;
+  origin: string;
+  code: string;
+};
+
+const Track = new Map<string, TrackInfo>();
+
+const LIMIT = 50;
 
 export class ExtensionWorker {
+  static getTrackedRequests() {
+    return Array.from(Track.values());
+  }
+
   #setMessageListener() {
     chrome.runtime.onMessage.addListener((...x) => {
       messageController.handleMessage(...x);
@@ -8,7 +26,50 @@ export class ExtensionWorker {
     });
   }
 
+  #setRequestListener() {
+    chrome.webRequest.onCompleted.addListener(
+      async (details) => {
+        if (!details.url.startsWith("http")) return;
+
+        const appConfig = await StorageController.get(
+          StorageKeyEnum.APP_CONFIG,
+          AppConfigSchema
+        );
+
+        if (!appConfig.sendNetwork || !appConfig.urlTrack) return;
+
+        const tabs = await chrome.tabs.query({ active: true });
+
+        if (!tabs || tabs.length === 0) return;
+
+        const activeTab = tabs[0];
+
+        if (details.tabId !== activeTab?.id) return;
+
+        if (details.initiator?.includes(appConfig.urlTrack)) {
+          Track.set(details.requestId, {
+            url: details.url,
+            createdAt: Date.now(),
+            origin: details.initiator || "unknown",
+            code: details.statusCode.toString(),
+          });
+
+          if (Track.size > LIMIT) {
+            const oldestKey = Array.from(Track.entries()).reduce((a, b) =>
+              a[1].createdAt < b[1].createdAt ? a : b
+            )[0];
+            Track.delete(oldestKey);
+          }
+        }
+      },
+      {
+        urls: ["<all_urls>"],
+      }
+    );
+  }
+
   init() {
+    this.#setRequestListener();
     this.#setMessageListener();
   }
 }
--- src/schemas/settings/app-config.ts
+++ src/schemas/settings/app-config.ts
@@ -3,6 +3,8 @@ import * as v from "valibot";
 export const AppConfigSchema = v.object({
   setup: v.boolean(),
   provider: v.string(),
+  sendNetwork: v.optional(v.boolean()),
+  urlTrack: v.optional(v.string()),
 });
 
 export type AppConfigSchema = v.InferOutput<typeof AppConfigSchema>;
