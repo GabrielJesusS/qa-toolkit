689ecaba50d23f1ef6dcdff2f76e858970941432
Gabriel Jesus <gabrieljesus9221@gmail.com>
2026-01-06 08:30:55 -0300

    feat: adds basic popup actions

---
+++ src/components/PopupActions.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import CogIcon from "@/assets/cog.svg?component";
+
+async function openSettings() {
+    const currentTab = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+    });
+
+
+    if (currentTab.length === 0) return;
+
+    const tab = currentTab[0];
+
+    if (!tab.id) return;
+
+
+    await chrome.sidePanel.open({
+        tabId: tab.id,
+    });
+}
+
+</script>
+
+<template>
+    <div class="qtk:flex qtk:gap-4">
+        <button @click="openSettings" type="button"
+            class="qtk:p-2 qtk:bg-gray-100 qtk:rounded-full qtk:transition-all qtk:duration-75 qtk:ease-in qtk:text-gray-400 qtk:cursor-pointer qtk:active:bg-primary-dark qtk:active:text-white">
+            <CogIcon class="qtk:size-6" />
+        </button>
+    </div>
+</template>
--- src/views/popup/App.vue
+++ src/views/popup/App.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
+
+import Logo from "@/assets/logo.svg?component"
+import PopupActions from "@/components/PopupActions.vue";
+
 </script>
 
 <template>
-  <div>
-    Pop up
+  <div class="qtk:bg-white qtk:w-xs qtk:p-4 qtk:flex qtk:flex-col qtk:gap-4">
+    <div class="qtk:flex qtk:gap-2 qtk:items-center">
+      <Logo class="qtk:size-8 qtk:text-primary" />
+      <h1 class="qtk:text-xl md:qtk:text-5xl lg:qtk:text-6xl qtk:font-extrabold qtk:text-primary">
+        QA Toolkit
+      </h1>
+    </div>
+    <PopupActions />
   </div>
 </template>
