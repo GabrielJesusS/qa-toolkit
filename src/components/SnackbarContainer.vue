bcf60ef44f687039177a813b6a17cf242d99e088
Gabriel Jesus <gabriel.jesus@novahaus.com.br>
2026-01-10 16:07:33 -0300

    feat: adds new snackbar configs

--- src/components/SnackbarContainer.vue
+++ src/components/SnackbarContainer.vue
@@ -1,22 +1,38 @@
 <script setup lang="ts">
 import { useSnackbar } from '@/composables/useSnackbar';
 import Toast from './Toast.vue';
+import { computed } from 'vue';
+import clsx from 'clsx';
+
+type Props = {
+    position?: 'top-right' | 'top-left' | 'bottom-right' | 'bottom-left';
+    large?: boolean;
+}
 
 const { notifications } = useSnackbar();
 
+const props = withDefaults(defineProps<Props>(), { position: 'bottom-right', large: false });
+
+const classes = computed(() => clsx('qtk:z-99999999 qtk:fixed qtk:transform qtk:flex qtk:flex-col qtk:space-y-2', {
+    'qtk:bottom-0 qtk:py-4 qtk:top-auto': props.position.startsWith('bottom'),
+    'qtk:top-0 qtk:py-4 qtk:bottom-auto': props.position.startsWith('top'),
+    'qtk:right-0 qtk:px-4 qtk:left-auto': props.position.endsWith('right'),
+    'qtk:left-0 qtk:px-4 qtk:right-auto': props.position.endsWith('left'),
+    'qtk:w-full': props.large,
+}))
+
 </script>
 
-
 <template>
-    <div
-        class="qtk:z-99999999 qtk:fixed qtk:bottom-4 qtk:right-16 qtk:transform qtk:flex qtk:flex-col qtk:space-y-2">
-        <ul class="qtk:flex qtk:flex-col qtk:gap-1">
+    <div :class="classes">
+        <ul class="qtk:flex qtk:flex-col qtk:gap-1 qtk:w-full">
             <TransitionGroup name="slide-fade">
                 <li v-for="notification in notifications" :key="notification.id">
                     <Toast :type="notification.type">
                         {{ notification.message }}
                     </Toast>
                 </li>
+
             </TransitionGroup>
         </ul>
     </div>
