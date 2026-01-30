0c3d49daaa55561c0bfc8d15f3b07266005c4cab
Gabriel Jesus <gabrieljesis9221@gmail.com>
2026-01-30 10:58:50 -0300

    fix: fixes login lock into first access

--- src/components/sections/Home.vue
+++ src/components/sections/Home.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import Standby from './Standby.vue';
 import InitialSetup from './InitialSetup.vue';
-import RevalidateSession from './RevalidateSession.vue';
 import { useConfig } from '@/composables/useConfig';
 
 const { config } = useConfig()
@@ -11,8 +10,7 @@ const { config } = useConfig()
 <template>
     <div class="qtk:max-h-screen qtk:overflow-hidden">
         <Transition name="slide-fade" mode="out-in">
-            <Standby v-if="!!config?.setup && config.provider && config.validSession" />
-            <RevalidateSession v-else-if="!config.validSession" />
+            <Standby v-if="!!config?.setup && config.provider" />
             <InitialSetup v-else />
         </Transition>
     </div>
