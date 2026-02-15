a531451f41c8f2df5114e0b7fa593542e027c719
Gabriel Jesus <gabrieljesus9221@gmail.com>
2026-02-15 14:03:49 -0300

    fix: fixes content button pointer

--- src/components/ContentButton.vue
+++ src/components/ContentButton.vue
@@ -16,7 +16,7 @@ const title = computed(() => `Take a screenshot ${hasUrlTrack.value ? `(Tracking
 
 <template>
     <button :title="title" :class="clsx('qtk:bg-white qtk:group/btn qtk:relative qtk:text-primary qtk:disabled:cursor-not-allowed qtk:disabled:text-gray-400 qtk:size-fit qtk:block qtk:p-1 qtk:rounded-full qtk:shadow-lg qtk:focus:qtk:outline-none qtk:cursor-pointer',
-        hasUrlTrack && 'qtk:after:size-8 qtk:after:animate-ping qtk:after:block qtk:after:bg-red-500/50 qtk:after:absolute qtk:after:inset-0 qtk:after:rounded-full'
+        hasUrlTrack && 'qtk:after:size-8 qtk:after:pointer-events-none qtk:after:animate-ping qtk:after:block qtk:after:bg-red-500/50 qtk:after:absolute qtk:after:inset-0 qtk:after:rounded-full'
     )">
         <Logo
             :class="clsx('qtk:size-6 qtk:text-current qtk:group-aria-busy/btn:opacity-0', hasUrlTrack && 'qtk:text-red-500',)" />
